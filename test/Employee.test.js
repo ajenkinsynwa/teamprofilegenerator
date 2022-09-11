f56baff2633a8b2b5556bec86e7e5544409739d7
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-09-10 23:12:24 -0400

    updated Engineer Test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,24 +1,24 @@
 const Employee = require("../lib/Employee");
 const employee = new Employee('emily', '987654', 'emilyann08@gmail.com');
 
-testobject('test the name, id, email for the emloyee object', () => {
+test('test the name, id, email for the emloyee object', () => {
     describe(employee.name).toBe('emily');
     describe(employee.id).toBe('987654');
     describe(emloyee.email).toBe('emilyann08@gmail.com')
 });
 
-testobject('test to get the name of employee from the getName() method', () => {
+test('test to get the name of employee from the getName() method', () => {
     describe(employee.getName()).toBe('emily');
 });
 
-testobject('test if we can get the id from the getId() method', () => {
-    expect(employee.getId()).toBe('987654');
+test('test if we can get the id from the getId() method', () => {
+    describe(employee.getId()).toBe('987654');
 });
 
-testobject('test if we can get the email from the getemail() method', () => {
-    expect(employee.getEmail()).toBe('emilyann08@gmail.com');
+test('test if we can get the email from the getemail() method', () => {
+    describe(employee.getEmail()).toBe('emilyann08@gmail.com');
 });
 
-testobject('test if we can get the role from the getrole() method', () => {
-    expect(employee.getrole()).toBe('Employee');
+test('test if we can get the role from the getrole() method', () => {
+    describe(employee.getrole()).toBe('Employee');
 });
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,5 +1,24 @@
 const Engineer = require("../lib/Engineer");
+const engineer = new Engineer('dana', '1234567', 'danamason@gmail.com');
 
-describe("Engineer", () => {
+test('test the name, id, email for the emloyee object', () => {
+    describe(engineer.name).toBe('dana');
+    describe(engineer.id).toBe('1234567');
+    describe(engineer.email).toBe('danamason@gmail.com')
+});
 
+test('test to get the name of employee from the getName() method', () => {
+    describe(engineer.getName()).toBe('dana');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    describe(engineer.getId()).toBe('1234567');
+});
+
+test('test if we can get the email from the getemail() method', () => {
+    describe(engineer.getEmail()).toBe('danamason@gmail.com');
+});
+
+test('test if we can get the role from the getrole() method', () => {
+    describe(engineer.getrole()).toBe('Engineer');
 });
